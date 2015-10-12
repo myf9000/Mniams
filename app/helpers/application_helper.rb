f1ba266ab4d696a6feede4c03b0e35bdfed90fed
myf9000 <myf9000@vp.pl>
2015-10-12 12:23:52 +0200

    Add working title page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def set_title_of_page(page_title="")
+		add = "Recipes"
+		if page_title.empty?
+			page_title = add
+		else
+			page_title = page_title+ " | " +add
+		end
+	end
 end
