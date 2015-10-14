09051e2219c56cd8ebdc03df4fbb88711c0ea224
myf9000 <myf9000@vp.pl>
2015-10-14 10:06:06 +0200

    Clean database for to fix Devise

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,16 @@
+class RegistrationsController < Devise::RegistrationsController
+private
+
+  def sign_up_params
+    params.require(:user).permit( :email, :password, :password_confirmation , :role)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :current_password , :role , :name)
+  end
+
+  devise_for :users, :controllers => { registrations: ‘registrations’ }
+
+
+  
+end
