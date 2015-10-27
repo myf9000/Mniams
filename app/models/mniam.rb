bb7b523a1a7cf13c0516584f72d0df07f141ce39
myf9000 <myf9000@vp.pl>
2015-10-27 09:24:50 +0100

    Styling edit user page

--- app/models/mniam.rb
+++ app/models/mniam.rb
@@ -1,7 +1,7 @@
 class Mniam < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments, dependent: :destroy
-	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "https://static.pexels.com/photos/2232/vegetables-italian-pizza-restaurant.jpg"
   	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   	acts_as_taggable_on :content, :name, :tag_list
 	acts_as_taggable
