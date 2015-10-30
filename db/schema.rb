19869e89e836720ef69445b5c9d05a924fd5ce11
myf900 <myf9000@vp.pl>
2015-10-30 17:05:24 +0100

    Set up email with devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151026151928) do
+ActiveRecord::Schema.define(version: 20151030160327) do
 
   create_table "comment_hierarchies", id: false, force: :cascade do |t|
     t.integer "ancestor_id",   null: false
@@ -183,8 +183,11 @@ ActiveRecord::Schema.define(version: 20151026151928) do
     t.string   "slug"
     t.integer  "rank"
     t.text     "desc"
+    t.string   "confirmable"
+    t.string   "confirmation_token"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   add_index "users", ["slug"], name: "index_users_on_slug", unique: true
